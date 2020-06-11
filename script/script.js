9024c030a31b496042f1381f5bcacf91857e80b0
Tetrapleg <rufmama@yandex.ru>
2020-06-11 17:59:56 +0300

    ДЗ 6азовое

---
+++ script/script.js
@@ -0,0 +1,62 @@
+
+'use strict';
+
+// let num = Math.ceil(Math.random() * 100);
+// console.log(num);
+
+// let persNum = prompt('Угадай число от 1 до 100');
+
+// function guessTheNumber(a) {
+
+//   if (!isNaN(parseFloat(a)) && isFinite(a)) {
+//       let ques;
+//       if (a > num) {
+//         ques = confirm('Загаданное число меньше, попробовать ещё?');
+//       } else if (a < num) {
+//         ques = confirm('Загаданное число больше, попробовать ещё?');
+//       } else {
+//         alert('Поздравляю, Вы угадали!!!');
+//       }
+//       if (ques === true) {
+//         return guessTheNumber(prompt('Введи число!'));
+//       }
+//   } else {
+//     return guessTheNumber(prompt('Введи число!'));
+//   } 
+// }
+
+// guessTheNumber(persNum);
+
+let num = Math.ceil(Math.random() * 100);
+console.log(num);
+
+function guessTheNumber() {
+
+  let persNum = prompt('Угадай число от 1 до 100');
+
+  return function() {
+    if (!isNaN(parseFloat(persNum)) && isFinite(persNum)) {
+      let ques;
+      if (persNum > num) {
+        ques = confirm('Загаданное число меньше, попробовать ещё?');
+      } else if (persNum < num) {
+        ques = confirm('Загаданное число больше, попробовать ещё?');
+      } else {
+        alert('Поздравляю, Вы угадали!!!');
+      }
+      if (ques === true) {
+        persNum = prompt('Введи число!');
+      }
+    } else {
+      persNum = prompt('Введи число!');
+    } 
+    return persNum;
+  };
+
+}
+
+const persVar = guessTheNumber();
+persVar();
+persVar();
+persVar();
+
