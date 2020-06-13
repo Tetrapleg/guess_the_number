901d3c0b9d1289691a649e99665996848b2d1d47
Tetrapleg <rufmama@yandex.ru>
2020-06-13 10:38:42 +0300

    исправление ДЗ №6

--- script/script.js
+++ script/script.js
@@ -1,67 +1,38 @@
 
 'use strict';
 
-// let num = Math.ceil(Math.random() * 100);
-// console.log(num);
-
-// let persNum = prompt('Угадай число от 1 до 100');
-
-// function guessTheNumber(a) {
-
-//   if (!isNaN(parseFloat(a)) && isFinite(a)) {
-//       let ques;
-//       if (a > num) {
-//         ques = confirm('Загаданное число меньше, попробовать ещё?');
-//       } else if (a < num) {
-//         ques = confirm('Загаданное число больше, попробовать ещё?');
-//       } else {
-//         alert('Поздравляю, Вы угадали!!!');
-//       }
-//       if (ques === true) {
-//         return guessTheNumber(prompt('Введи число!'));
-//       }
-//   } else {
-//     return guessTheNumber(prompt('Введи число!'));
-//   } 
-// }
-
-// guessTheNumber(persNum);
-
-
-
 function guessTheNumber() {
 
   let num = Math.ceil(Math.random() * 100);
   console.log(num);
 
   let persNum = prompt('Угадай число от 1 до 100');
+  console.log(typeof (persNum));
+  console.log(persNum);
 
-  return function() {
+  function startGame() {
     if (!isNaN(parseFloat(persNum)) && isFinite(persNum)) {
-      let ques;
-      if (persNum > num) {
-        ques = confirm('Загаданное число меньше, попробовать ещё?');
-      } else if (persNum < num) {
-        ques = confirm('Загаданное число больше, попробовать ещё?');
-      } else {
-        alert('Поздравляю, Вы угадали!!!');
-      }
-      if (ques === true) {
-        persNum = prompt('Введи число!');
-      }
+        let ques;
+        if (persNum > num) {
+          ques = confirm('Загаданное число меньше, попробовать ещё?');
+        } else if (persNum < num) {
+          ques = confirm('Загаданное число больше, попробовать ещё?');
+        } else {
+          alert('Поздравляю, Вы угадали!!!');
+        }
+        if (ques === true) {
+          persNum = (prompt('Введи число!'));
+          return startGame();
+        }
     } else {
-      persNum = prompt('Введи число!');
+      persNum = (prompt('Введи число!'));
+      return startGame();
     } 
-    return persNum;
-  };
-
+  }
+ return startGame();
 }
 
 const persVar = guessTheNumber();
-persVar();
-persVar();
-persVar();
-console.log(persVar);
 
 
 
