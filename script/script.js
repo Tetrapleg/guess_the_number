8629725afac78cc377c8bbefc0dc9284ded6f406
Tetrapleg <rufmama@yandex.ru>
2020-06-13 14:30:53 +0300

    ДЗ №6 повышенной сложности

--- script/script.js
+++ script/script.js
@@ -4,21 +4,32 @@
 function guessTheNumber() {
 
   let num = Math.ceil(Math.random() * 100);
-  console.log(num);
 
-  let persNum = prompt('Угадай число от 1 до 100');
-  console.log(typeof (persNum));
-  console.log(persNum);
+  let persNum = prompt('Угадай число от 1 до 100, осталось попыток - 10');
+
+  let n = 10;
 
   function startGame() {
+
     if (!isNaN(parseFloat(persNum)) && isFinite(persNum)) {
-        let ques;
-        if (persNum > num) {
-          ques = confirm('Загаданное число меньше, попробовать ещё?');
-        } else if (persNum < num) {
-          ques = confirm('Загаданное число больше, попробовать ещё?');
+        n--;
+        let ques,
+            newStart;
+
+        if (persNum > num && n > 0) {
+          ques = confirm('Загаданное число меньше, осталось попыток - ' + n + '  Продолжим?');
+        } else if (persNum < num && n > 0) {
+          ques = confirm('Загаданное число больше, осталось попыток - ' + n + '  Продолжим?');
+        } else if (n > 0) {
+          newStart = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+          if (newStart === true) {
+            return guessTheNumber();
+          }
         } else {
-          alert('Поздравляю, Вы угадали!!!');
+          newStart = confirm('Ваши попытки закончились, хотите играть заново?');
+          if (newStart === true) {
+          return guessTheNumber();
+          }
         }
         if (ques === true) {
           persNum = (prompt('Введи число!'));
